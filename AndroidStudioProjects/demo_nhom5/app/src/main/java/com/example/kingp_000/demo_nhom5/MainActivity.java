5ffeaa6cee04d31d3b324669508c0fbd0d47d2aa
Pham Quang <kingpq95@gmail.com>
2016-11-07 22:45:26 +0700

    demo

---
+++ AndroidStudioProjects/demo_nhom5/app/src/main/java/com/example/kingp_000/demo_nhom5/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.kingp_000.demo_nhom5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
